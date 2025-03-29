fdf3109466f1e0e8ad7a1cf1a7b03e400cd475f7
Kulan66 <kulanthamuditha66@gmail.com>
2025-03-29 23:00:06 +0530

    .

--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,28 @@ import "dotenv/config";
 import express from "express";
 import connectDB from "./infrastructure/db";
 
+
+
 import { clerkMiddleware } from "@clerk/express";
 import cors from "cors";
 import bookingsRouter from "./api/booking";
 import hotelsRouter from "./api/hotel";
 import globalErrorHandlingMiddleware from "./api/middlewares/global-error-handling-middleware";
 
-// Create an Express instance
 const app = express();
 
 app.use(clerkMiddleware());
-// Middleware to parse JSON data in the request body
-app.use(express.json());
-app.use(cors({ origin: "https://kulanthamuditha.netlify.app" }));
 
-// app.use((req, res, next) => {
-//   console.log("Hello World");
-//   next();
-// });
+app.use(express.json());
+app.use(cors());
+
+connectDB();
 
 app.use("/api/hotels", hotelsRouter);
 app.use("/api/bookings", bookingsRouter);
 
 app.use(globalErrorHandlingMiddleware);
 
-// Define the port to run the server
-connectDB();
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = 8000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
 
